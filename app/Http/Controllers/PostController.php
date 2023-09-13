73229f28637ac362b1c6b40c7035e5049a72d066
nanasekoyama <s193033gpwu@gmail.com>
2023-09-13 12:16:43 +0000

    blog 17

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,8 +9,17 @@ class PostController extends Controller
 {
    public function index(Post $post)
    {
-       return $post->get();
+       return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
    }
-    //
+   
+   public function show(Post $post)
+{
+    return view('posts/show')->with(['post' => $post]);
+ //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
 }
  
+ public function create()
+ {
+     return view('posts/create');
+ }
+}
