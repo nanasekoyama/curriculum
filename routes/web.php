09787cac738b86abc50074c678fa7706a26f1b30
nanasekoyama <s193033gpwu@gmail.com>
2023-10-03 08:21:36 +0000

    投稿の削除

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/posts/create', [PostController::class,'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::put('posts/{post}', [PostControtter::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class,'delete']);
